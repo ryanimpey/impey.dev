dc4da681e30360fd66ce20c3003f70413babe87a
Ryan Impey <ryanimpey@live.co.uk>
2019-10-13 22:22:11 +0100

    Added about pages

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+    <Layout>
+        <SEO title="About" />
+        <h1>Hi there!</h1>
+        <p>
+            I'm still building this site, so for now all that exists is this
+            page. You can find more at my <a href="https://github.com/ryanimpey" target="_blank" rel="noopener noreferrer">Github</a> until this is complete.
+        </p>
+    </Layout>
+)
+
+export default IndexPage
