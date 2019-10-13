d001ef2748d0c569d4cf5b849955a1c734f46429
Ryan Impey <ryanimpey@live.co.uk>
2019-10-13 16:05:09 +0100

    Updates

---
+++ component---src-pages-404-js-05c90bc1bc6e10b78b58.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{217:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(216),o=n(215);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-05c90bc1bc6e10b78b58.js.map
