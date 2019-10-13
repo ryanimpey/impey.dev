666b0310bc1d735bbc4bdda22236f60781dbcda8
Ryan Impey <ryanimpey@live.co.uk>
2019-10-13 22:23:12 +0100

    Updates

---
+++ component---src-pages-page-2-js-6995c35b1b9c22d87531.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{220:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(55),c=a(216),r=a(215);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-6995c35b1b9c22d87531.js.map
