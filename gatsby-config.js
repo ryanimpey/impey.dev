16cabb433e39d96d0794c9e9b481e09b4612ed4b
Ryan Impey <ryanimpey@live.co.uk>
2019-10-15 14:31:17 +0100

    Config updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        title: `Ryan Impey`,
+        description: `Portfolio page for Ryan Impey - Junior fullstack developer with industry experience in the Javascript Ecosystem`,
+        author: `ryanimpey - Github`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
