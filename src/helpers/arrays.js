485fe086667fcf1b56fd5f6535b19d13bf06d118
Ryan <ryanimpey@live.co.uk>
2020-01-07 22:45:05 +0000

    Added project posters to arrays helper

--- src/helpers/arrays.js
+++ src/helpers/arrays.js
@@ -1,3 +1,11 @@
+// Poster imports
+import temmt_poster from "../images/posters/temmt.png";
+import skipper_poster from "../images/posters/skipper.png";
+import wstraining_poster from "../images/posters/wstraining.jpg";
+import talkable_poster from "../images/posters/talkable.png";
+import navigateme_poster from "../images/posters/navigateme.png";
+import findapc_poster from "../images/posters/findapc.png";
+
 export const nav_links = [
     {
         name: "Projects",
@@ -42,36 +50,42 @@ export const projects = [
     {
         name: "Temmt",
         description:
-            "A location based mobile application providing deals and discounts to users as they walk along the high street.",
+            "A location-based mobile application providing deals and discounts to users as they walk along the high street.",
         skills: ["React", "React Native", "Redux", "Firebase"],
+        image: temmt_poster,
     },
     {
         name: "Skipper My Boat",
         description:
             "A Yacht and boat rental platform offering peer-to-peer rental between individual owners/agencies and travellers.",
         skills: ["React", "React Native", "Redux", "Firebase", "NoSQL", "MongoDB", "Express", "Node.js"],
+        image: skipper_poster,
     },
     {
         name: "WS Training",
         description: "A clean, modern website and admin management system for a local apprenticeship company.",
         skills: ["HTML5", "CSS", "Express", "Bootstrap", "Handlebars", "NoSQL"],
+        image: wstraining_poster,
     },
     {
         name: "Talkable",
         description:
             "A Startup medical platform allowing users to easily find providers that specialises in what you need help with.",
         skills: ["Vue", "Bootstrap", "Firebase"],
+        image: talkable_poster,
     },
     {
         name: "Find-a-PC",
         description:
             "A University of Lincoln based project helping students find free/unused computers in campus buildings.",
         skills: ["Leaflet", "GeoJSON", "HTML5", "Bootstrap", ".NET"],
+        image: navigateme_poster,
     },
     {
         name: "Navigate-Me",
         description:
             "A University of Lincoln based project helping students navigate around the campus by providing pathfinding with an interactive map.",
         skills: ["Leaflet", "GeoJSON", "HTML5", "Bootstrap", ".NET"],
+        image: findapc_poster,
     },
 ];
