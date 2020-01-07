a9536aabf625c5e27cbb31001d40c749d3ace79e
Ryan <ryanimpey@live.co.uk>
2020-01-07 00:06:41 +0000

    CSS Global style/ styled-components updates

---
+++ src/styles/pages/indexPageStyle.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const LinkContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 96px;
+`;
+
+const Section = styled.section`
+    padding-bottom: 64px;
+`;
+
+export default {
+    Section,
+    LinkContainer,
+};
