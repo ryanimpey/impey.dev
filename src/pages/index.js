8e4f49bc5c932feed579140ce4d930e4cd5769bd
Ryan <ryanimpey@live.co.uk>
2020-01-07 22:44:43 +0000

    Desktop page completion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Carousel from "nuka-carousel";
-import { graphql, useStaticQuery } from "gatsby";
 import { ThemeProvider } from "styled-components";
 import { FiGithub, FiLinkedin, FiMail, FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { Grid, Row, Col } from "react-flexbox-grid";
 
 import SEO from "../components/seo";
 import Badge from "../components/badge";
@@ -20,7 +20,6 @@ import { experiences, certifications, projects } from "../helpers/arrays";
 const IndexPage = () => {
     // State management for current project being displayed
     const [currProject, setCurrProject] = useState(0);
-
     // Hold current slide value in state
     const updateCurrentProject = (prev, next) => setCurrProject(next);
 
@@ -28,7 +27,7 @@ const IndexPage = () => {
         <ThemeProvider theme={dark_theme}>
             <Layout>
                 <SEO title="Home" />
-                <Index.Section>
+                <Index.IntroSection>
                     <H1>
                         Hello,
                         <br />
@@ -36,38 +35,55 @@ const IndexPage = () => {
                     </H1>
                     <P>{intro_message}</P>
                     <SocialLinks />
-                </Index.Section>
-                <Index.CarouselSection className="index--carousel--section">
+                </Index.IntroSection>
+                <Index.CarouselSection>
                     <H2>Projects</H2>
-                    <Carousel
-                        wrapAround
-                        className="index--carousel"
-                        renderCenterLeftControls={({ previousSlide }) => (
-                            <FiChevronLeft style={{cursor: 'pointer'}} onClick={previousSlide} size={24} />
-                        )}
-                        renderCenterRightControls={({ nextSlide }) => <FiChevronRight style={{cursor: 'pointer'}} onClick={nextSlide} size={24} />}
-                        beforeSlide={updateCurrentProject}
-                    >
-                        <img src="https://via.placeholder.com/376x188/ffffff/c0392b" />
-                        <img src="https://via.placeholder.com/376x188/ffffff/c0392b" />
-                        <img src="https://via.placeholder.com/376x188/ffffff/c0392b" />
-                        <img src="https://via.placeholder.com/376x188/ffffff/c0392b" />
-                        <img src="https://via.placeholder.com/376x188/ffffff/c0392b" />
-                        <img src="https://via.placeholder.com/376x188/ffffff/c0392b" />
-                    </Carousel>
-                    <H3>{projects[currProject].name}</H3>
-                    <P className="margin--top--none">{projects[currProject].description}</P>
-                    <Index.BadgeHolder>
-                        {projects[currProject].skills.map(skill => (
-                            <Badge>{skill}</Badge>
-                        ))}
-                    </Index.BadgeHolder>
+                    <div className="index--carousel--container">
+                        <Carousel
+                            wrapAround
+                            className="index--carousel"
+                            renderCenterLeftControls={({ previousSlide }) => (
+                                <FiChevronLeft style={{ cursor: "pointer" }} onClick={previousSlide} size={24} />
+                            )}
+                            renderCenterRightControls={({ nextSlide }) => (
+                                <FiChevronRight style={{ cursor: "pointer" }} onClick={nextSlide} size={24} />
+                            )}
+                            beforeSlide={updateCurrentProject}
+                        >
+                            {projects.map(({ image, name }, index) => (
+                                <img alt={name} src={image} key={String(index)} />
+                            ))}
+                        </Carousel>
+                        <H3>{projects[currProject].name}</H3>
+                        <P className="margin--top--none">{projects[currProject].description}</P>
+                        <Index.BadgeHolder>
+                            {projects[currProject].skills.map(skill => (
+                                <Badge key={skill}>{skill}</Badge>
+                            ))}
+                        </Index.BadgeHolder>
+                    </div>
+                    <Grid style={{ paddingLeft: 0, paddingRight: 0 }} className="index--projectgrid-container">
+                        <Row style={{ marginTop: 16 }}>
+                            {projects.map(({ name, description, skills, image }) => (
+                                <Col md={4} style={{ marginBottom: 16 }}>
+                                    <Index.ProjectImage src={image} />
+                                    <H3>{name}</H3>
+                                    <P>{description}</P>
+                                    <Index.BadgeHolder>
+                                        {skills.map(skill => (
+                                            <Badge key={skill}>{skill}</Badge>
+                                        ))}
+                                    </Index.BadgeHolder>
+                                </Col>
+                            ))}
+                        </Row>
+                    </Grid>
                 </Index.CarouselSection>
                 <Index.Section>
                     <H2>Experience</H2>
                     <Index.ListContainer>
-                        {experiences.map(({ title, location, duration }) => (
-                            <ListItem>
+                        {experiences.map(({ title, location, duration }, index) => (
+                            <ListItem key={String(index)}>
                                 <H4>{title}</H4>
                                 <P>{location}</P>
                                 <small>{duration}</small>
@@ -78,8 +94,8 @@ const IndexPage = () => {
                 <Index.Section>
                     <H2>Certifications</H2>
                     <Index.ListContainer>
-                        {certifications.map(certification => (
-                            <ListItem>
+                        {certifications.map((certification, index) => (
+                            <ListItem key={String(index)}>
                                 <H4>{certification}</H4>
                             </ListItem>
                         ))}
@@ -108,18 +124,5 @@ const SocialLinks = () => (
         </a>
     </Index.LinkContainer>
 );
-
-const imageQuery = graphql`
-    query retrieveMainImage {
-        image: file(relativePath: { eq: "mountain.jpg" }) {
-            id
-            childImageSharp {
-                fluid {
-                    ...GatsbyImageSharpFluid
-                }
-            }
-        }
-    }
-`;
 
 export default IndexPage;
