a97d49f3f6d1bbffb288fcf1ae753b7ef4f9f32d
Ryan <ryanimpey@live.co.uk>
2020-01-07 00:05:03 +0000

    Added styled-components ThemeProvider to Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,17 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./header/index"
-import "./layout.css"
-import "../styles/_variables.scss"
-import "../styles/_globals.scss"
+import Header from "./header/header";
+
+import "normalize.css";
+import "../styles/global.css";
+import { ThemeProvider } from "styled-components";
+import { dark_theme } from "../styles/themes";
+import { Container } from "../styles/components/layoutStyle";
 
 const Layout = ({ children }) => {
     const data = useStaticQuery(graphql`
@@ -23,18 +26,18 @@ const Layout = ({ children }) => {
                 }
             }
         }
-    `)
+    `);
 
     return (
-        <>
+        <ThemeProvider theme={dark_theme}>
             <Header siteTitle={data.site.siteMetadata.title} />
-            <main>{children}</main>
-        </>
-    )
-}
+            <Container>{children}</Container>
+        </ThemeProvider>
+    );
+};
 
 Layout.propTypes = {
     children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
---
+++ src/styles/themes.js
@@ -0,0 +1,10 @@
+// Dark Theme Styling
+export const dark_theme = {
+    background: "#272727",
+    backgroundAlt: "white",
+    textColor: "white",
+    textColorAlt: "#272727",
+};
+
+// Light Theme Styling
+export const light_theme = {};
