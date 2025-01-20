509d01a0db74fdf105a5ff8e0bbc10d3f2f80195
Ryan Impey <impey.ryan@outlook.com>
2025-01-20 21:08:14 +0000

    Updates

---
+++ _astro/no-one-is-coming-to-save-you.gKdx8yVS.js
@@ -0,0 +1,40 @@
+const e="no-one-is-coming-to-save-you.md",n="post",t="no-one-is-coming-to-save-you",o=`
+## No one is coming to save you.
+
+I'm rubbish at sharing stuff online. If it's not perfect, I don't want to share it. Maybe I'm a perfectionist, maybe I'm still the teenager who didn't enjoy my mates finding my 5 year old minecraft tutorials back in 2016. Fun times.
+
+Anyway.
+
+I have this little blue Moleskine notebook that I take with me anywhere my laptop goes, it's my nice junk and thinking booklet. I've recently stopped using it in favour of another more organised black Moleskine to document my daily progress, really exciting stuff I know, but the first page of it reads as follows:
+
+\`\`\`yaml
+Getting the path defined, the 1:5:10 strategy
+
+- 5 Years from now
+- - Building good investment portfolio
+- - Income from multiple streams
+- - Successful / mainstream SaaS / App
+- - Mortgage heavily reduced
+- - £500k net worth
+
+
+Defined on 08 Aug 2022, checking back in 2023
+\`\`\`
+
+I wrote these straight after coming back and re-reading \`The Millionaire Fastlane\` by MJ DeMarco on holiday, really determined to hit these, and leave my corporate job for something entirely self-made.
+
+If you can't tell, we're not in 2023 anymore, shit we're not even in 2024. I'm no where near hitting these goals, other than the leaving corporate, but that's only because my entire team was laid off, thanks lads.
+
+Throughout 2023 and 2024 I spent countless evenings and weekends building things, making prototypes, getting products _ready_ to release, then bailing when I saw competition, doubting how a newcomer could enter an existing market, concerned that my product would be considered shit, even though I was using it to help me build things on a daily basis.
+
+I've become comfortably uncomfortable. Uncomfortable enough to try building something each night and weekend, but comfortable enough to not see it through, in case it's considered _cringe_ I guess, hence the reason for this big yap.
+
+Nothing more to say for now. Hopefully this kicks me into focusing on what I need to do, and seeing it through to release.
+
+### Thanks!
+`,i={title:"No one is coming to save you.",description:"Maybe the beginning of my online ramblings, read at the expense of your time.",publishDate:new Date(17373312e5),draft:!1,tags:["cringe"],ogImage:"/social-card.png"},a={type:"content",filePath:"/Users/ryanimpey/Documents/github/impey.dev/src/content/post/no-one-is-coming-to-save-you.md",rawData:`
+title: "No one is coming to save you."
+publishDate: "20 January 2025"
+description: "Maybe the beginning of my online ramblings, read at the expense of your time."
+tags: ["cringe"]
+ogImage: "/social-card.png"`};export{a as _internal,o as body,n as collection,i as data,e as id,t as slug};
