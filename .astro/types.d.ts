51e96c558b5a2f7f9482d47d33faeae85b020a63
Ryan Impey <impey.ryan@outlook.com>
2025-02-23 21:28:26 +0000

    Portfolio update

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -236,20 +236,6 @@ declare module 'astro:content' {
   collection: "post";
   data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
-"no-one-is-coming-to-save-you.md": {
-	id: "no-one-is-coming-to-save-you.md";
-  slug: "no-one-is-coming-to-save-you";
-  body: string;
-  collection: "post";
-  data: InferEntrySchema<"post">
-} & { render(): Render[".md"] };
-"tuesday-21-jan.md": {
-	id: "tuesday-21-jan.md";
-  slug: "tuesday-21-jan";
-  body: string;
-  collection: "post";
-  data: InferEntrySchema<"post">
-} & { render(): Render[".md"] };
 "webmentions/index.md": {
 	id: "webmentions/index.md";
   slug: "webmentions";
--- dist/_astro/index.CauwtR5g.js
+++ dist/_astro/index.CauwtR5g.js
@@ -1,4 +1,4 @@
-import{c as o,r as n,m as i}from"./render-template.Dqv0hIb6.js";import{u as a}from"./hoisted.BFWYe-Q9.js";import"./astro/assets-service.3IbH0_Ak.js";const t=`<h2 id="tldr">tldr</h2>
+import{c as o,r as n,m as i}from"./render-template.BhXifGpr.js";import{u as a}from"./hoisted.CH3cQFQR.js";import"./astro/assets-service.3IbH0_Ak.js";const t=`<h2 id="tldr">tldr</h2>
 <ol>
 <li>Ensure your social media links are added in <code>src/components/SocialList.astro</code>, most importantly the email address</li>
 <li>Create an account @ <a href="https://webmention.io/" rel="nofollow, noopener, noreferrer" target="_blank">Webmention.io</a>.</li>
