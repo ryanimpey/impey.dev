1cb8044ab9af12035f54242add0474cd4d7c7144
Ryan Impey <impey.ryan@outlook.com>
2025-02-23 21:28:32 +0000

    Updates

--- _astro/youchoose-privacy.DiBWGZiU.js
+++ _astro/youchoose-privacy.DiBWGZiU.js
@@ -1,5 +1,5 @@
-import{c as o,r as t,m as i}from"./render-template.Dqv0hIb6.js";import{u as r}from"./hoisted.BFWYe-Q9.js";import"./astro/assets-service.3IbH0_Ak.js";const n=`<h1 id="privacy-policy">Privacy Policy</h1>
-<p>One Percent (“we,” “our,” or “us”) is committed to protecting your privacy. This Privacy Policy outlines how we collect, use, and protect the personal information of users (“you”) of our application (the “App”).</p>
+import{c as n,r as t,m as i}from"./render-template.BhXifGpr.js";import{u as r}from"./hoisted.CH3cQFQR.js";import"./astro/assets-service.3IbH0_Ak.js";const o=`<h1 id="privacy-policy">Privacy Policy</h1>
+<p>You Choose (“we,” “our,” or “us”) is committed to protecting your privacy. This Privacy Policy outlines how we collect, use, and protect the personal information of users (“you”) of our application (the “App”).</p>
 <hr>
 <h2 id="information-we-collect">Information We Collect</h2>
 <p>We collect the following types of information:</p>
@@ -45,12 +45,6 @@ import{c as o,r as t,m as i}from"./render-template.Dqv0hIb6.js";import{u as r}fr
 </ul>
 </li>
 <li>
-<p><strong>Consent to Share Consumption Data with Apple</strong></p>
-<ul>
-<li>By using our app and making in-app purchases, you consent to our sharing of data regarding your usage and consumption of purchased content with Apple, as part of our efforts to resolve refund requests. This information may include details about how you have accessed and interacted with the purchased content. The purpose of sharing this data is to help Apple make an informed decision regarding refund requests. We ensure that such data sharing is done in compliance with Apple’s policies and only as necessary to process your requests.</li>
-</ul>
-</li>
-<li>
 <p><strong>As Required by Law</strong></p>
 <ul>
 <li>We may disclose your information if required to comply with legal obligations or to protect our legal rights.</li>
@@ -68,7 +62,7 @@ import{c as o,r as t,m as i}from"./render-template.Dqv0hIb6.js";import{u as r}fr
 <li>Requesting deletion of your data.</li>
 <li>Opting out of certain data processing activities.</li>
 </ul>
-<p>To exercise these rights, please contact us at <a href="mailto:onepercent@impey.dev">onepercent@impey.dev</a>.</p>
+<p>To exercise these rights, please contact us at <a href="mailto:youchoose@impey.dev">youchoose@impey.dev</a>.</p>
 <hr>
 <h2 id="third-party-services">Third-Party Services</h2>
 <p>This Privacy Policy applies solely to our App. Our App may contain links to third-party services, which are governed by their respective privacy policies. We encourage you to review their terms before interacting with their services.</p>
@@ -78,11 +72,11 @@ import{c as o,r as t,m as i}from"./render-template.Dqv0hIb6.js";import{u as r}fr
 <hr>
 <h2 id="contact-us">Contact Us</h2>
 <p>If you have any questions about this Privacy Policy or our data practices, please contact us at:<br>
-<strong>Email:</strong> <a href="mailto:onepercent@impey.dev">onepercent@impey.dev</a></p>
-<hr>`,a={title:"'One Percent' Privacy Policy",description:"Privacy policy for One Percent iOS & Android",publishDate:"27 November 2024",tags:["onepercent"],draft:!1,minutesRead:"3 min read"},s="/Users/ryanimpey/Documents/github/impey.dev/src/content/policy/onepercent-privacy.md",c=void 0;function m(){return`
+<strong>Email:</strong> <a href="mailto:youchoose@impey.dev">youchoose@impey.dev</a></p>
+<hr>`,a={title:"You Choose Privacy Policy",description:"Privacy policy for You Choose iOS & Android",publishDate:"27 November 2024",tags:["youchoose"],draft:!1,minutesRead:"2 min read"},s="/Users/ryanimpey/Documents/github/impey.dev/src/content/policy/youchoose-privacy.md",c=void 0;function g(){return`
 # Privacy Policy
 
-One Percent ("we," "our," or "us") is committed to protecting your privacy. This Privacy Policy outlines how we collect, use, and protect the personal information of users ("you") of our application (the "App").
+You Choose ("we," "our," or "us") is committed to protecting your privacy. This Privacy Policy outlines how we collect, use, and protect the personal information of users ("you") of our application (the "App").
 
 ---
 
@@ -120,11 +114,7 @@ We do not sell, rent, or trade your personal information. However, we may share 
 
     - We share purchase-related information with RevenueCat to process subscriptions, manage billing, and handle purchase validations. You can learn more about their privacy practices by visiting [RevenueCat's Privacy Policy](https://www.revenuecat.com/privacy).
 
-2. **Consent to Share Consumption Data with Apple**
-
-    - By using our app and making in-app purchases, you consent to our sharing of data regarding your usage and consumption of purchased content with Apple, as part of our efforts to resolve refund requests. This information may include details about how you have accessed and interacted with the purchased content. The purpose of sharing this data is to help Apple make an informed decision regarding refund requests. We ensure that such data sharing is done in compliance with Apple's policies and only as necessary to process your requests.
-
-3. **As Required by Law**
+2. **As Required by Law**
     - We may disclose your information if required to comply with legal obligations or to protect our legal rights.
 
 ---
@@ -143,7 +133,7 @@ Depending on your location, you may have rights regarding your personal informat
 -   Requesting deletion of your data.
 -   Opting out of certain data processing activities.
 
-To exercise these rights, please contact us at onepercent@impey.dev.
+To exercise these rights, please contact us at youchoose@impey.dev.
 
 ---
 
@@ -162,7 +152,7 @@ We may update this Privacy Policy periodically. Any changes will be posted in th
 ## Contact Us
 
 If you have any questions about this Privacy Policy or our data practices, please contact us at:  
-**Email:** onepercent@impey.dev
+**Email:** youchoose@impey.dev
 
 ---
-`}function f(){return n}function v(){return[{depth:1,slug:"privacy-policy",text:"Privacy Policy"},{depth:2,slug:"information-we-collect",text:"Information We Collect"},{depth:2,slug:"how-we-use-your-information",text:"How We Use Your Information"},{depth:2,slug:"sharing-your-information",text:"Sharing Your Information"},{depth:2,slug:"data-security",text:"Data Security"},{depth:2,slug:"your-rights",text:"Your Rights"},{depth:2,slug:"third-party-services",text:"Third-Party Services"},{depth:2,slug:"updates-to-this-policy",text:"Updates to This Policy"},{depth:2,slug:"contact-us",text:"Contact Us"}]}const w=o((u,p,l)=>{const{layout:h,...e}=a;return e.file=s,e.url=c,t`${i()}${r(n)}`});export{w as Content,f as compiledContent,w as default,s as file,a as frontmatter,v as getHeadings,m as rawContent,c as url};
+`}function f(){return o}function v(){return[{depth:1,slug:"privacy-policy",text:"Privacy Policy"},{depth:2,slug:"information-we-collect",text:"Information We Collect"},{depth:2,slug:"how-we-use-your-information",text:"How We Use Your Information"},{depth:2,slug:"sharing-your-information",text:"Sharing Your Information"},{depth:2,slug:"data-security",text:"Data Security"},{depth:2,slug:"your-rights",text:"Your Rights"},{depth:2,slug:"third-party-services",text:"Third-Party Services"},{depth:2,slug:"updates-to-this-policy",text:"Updates to This Policy"},{depth:2,slug:"contact-us",text:"Contact Us"}]}const w=n((u,l,p)=>{const{layout:h,...e}=a;return e.file=s,e.url=c,t`${i()}${r(o)}`});export{w as Content,f as compiledContent,w as default,s as file,a as frontmatter,v as getHeadings,g as rawContent,c as url};
