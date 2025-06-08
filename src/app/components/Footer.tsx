4514af4e02e7f83b5b674ea1f5af8565f1478872
Ryan Impey <impey.ryan@outlook.com>
2025-06-08 12:52:54 +0100

    Fix: footer date

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,7 @@ const year = new Date().getFullYear();
 const Footer = (props: Props) => {
     return (
         <footer className="max-w-xl text-sm mx-auto pb-8 bg-background font-mono">
-            &copy; Ryan Impey {year}, built with Redwood SDK
+            &copy; Ryan Impey 2025, built with Redwood SDK
         </footer>
     );
 };
