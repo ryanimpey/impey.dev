14b231566991883f520292185e7222527e18a6ba
Ryan Impey <ryanimpey@live.co.uk>
2025-08-29 20:14:59 +0100

    Added block rendering

--- app/[tenant]/[...slug]/page.tsx
+++ app/[tenant]/[...slug]/page.tsx
@@ -1,9 +1,42 @@
-export default async function Page({
-    params: paramsPromise,
-}: {
-    params: Promise<{ slug?: string[]; tenant: string }>;
-}) {
-    console.log("params::", await paramsPromise);
+import BlockRendererClient from "@/components/shared/BlocksRenderer";
+import { strapi } from "@strapi/client";
+import { notFound } from "next/navigation";
+const client = strapi({
+    baseURL: "https://fabulous-horses-b63e2ca388.strapiapp.com/api",
+    auth: process.env.STRAPI_API_KEY,
+});
 
-    return <p>Hello World!</p>;
+export default async function Page({ params }: { params: Promise<{ slug: string[]; tenant: string }> }) {
+    const { tenant, slug } = await params;
+    if (slug.length === 1) {
+        return notFound();
+    }
+
+    const articles = await client.collection("articles");
+    const all = await articles.find({
+        filters: {
+            slug: {
+                $eq: slug.at(1),
+            },
+        },
+    });
+
+    if (all.meta.pagination?.total === 0) {
+        return notFound();
+    }
+
+    const article = await articles.findOne(all.data.at(0)!.documentId);
+    const updatedAt = new Date(article.data.updatedAt);
+
+    return (
+        <main className="py-12">
+            <section className="max-w-xl mx-auto">
+                <h1 className="text-3xl lg:text-3xl font-bold leading-tight md:leading-tight">{article.data.title}</h1>
+                <h6 className="leading-tight md:leading-tight">Last updated {updatedAt.toDateString()}</h6>
+                <article className="py-8">
+                    <BlockRendererClient content={article.data.content} />
+                </article>
+            </section>
+        </main>
+    );
 }
